5930deeeccfa6ee8cb10b3a4a423125d689ec034
mjx <1032512598@qq.com>
2018-10-06 21:40:15 +0800

    add YHII

---
+++ src/main/java/Array/Summary/YHTriangleII.java
@@ -0,0 +1,22 @@
+package Array.Summary;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class YHTriangleII {
+
+    public List<Integer> getRow(int rowIndex) {
+        Integer[] row = new Integer[rowIndex+1];
+        for (int i=0;i<=rowIndex;i++){
+            row[i]=1;
+            for (int j =i-1;j>0;j--){
+                row[j] = row[j]+row[j-1];
+            }
+        }
+        return Arrays.asList(row);
+    }
+
+    public static void main(String[] args){
+
+    }
+}
