f85c2610e3c4b00cc111d2ecdbc50fac94b801a0
mjx <1032512598@qq.com>
2018-10-06 16:03:35 +0800

    finish MaxConsecutiveOne

--- src/main/java/Array/MaxConsecutiveOne.java
+++ src/main/java/Array/MaxConsecutiveOne.java
@@ -3,9 +3,17 @@ package Array;
 public class MaxConsecutiveOne {
 
     public int findMaxConsecutiveOnes(int[] nums) {
-
         int count = 0;
-
-        return count;
+        int max = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                count++;
+            }else {
+                max = Math.max(max,count);
+                count=0;
+            }
+        }
+        max = Math.max(max,count);
+        return max;
     }
 }
