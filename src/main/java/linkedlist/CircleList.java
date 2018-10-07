b9096bd0caa7ccd38f02fe4cbc87ce716a3892f6
mjx <1032512598@qq.com>
2018-10-07 21:15:32 +0800

    add CircleList

---
+++ src/main/java/linkedlist/CircleList.java
@@ -0,0 +1,34 @@
+package linkedlist;
+
+public class CircleList {
+    public boolean hasCycle(ListNode head) {
+        if (head == null||head.next==null)
+            return false;
+        ListNode temp = head;
+        while (temp != null&&temp.next!=null) {
+            head = head.next;
+            temp = temp.next;
+            if (temp == head) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        ListNode ln = new ListNode(3);
+        ln.next = ln;
+        CircleList cl = new CircleList();
+        System.out.println(cl.hasCycle(ln));
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
