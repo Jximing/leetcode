e7cb1ea4b0e3c88cb34c4ddf0a83092545d77a0f
mjx <1032512598@qq.com>
2018-10-08 21:08:11 +0800

    add IntersectionNode

---
+++ src/main/java/linkedlist/IntersectionNode.java
@@ -0,0 +1,26 @@
+package linkedlist;
+
+public class IntersectionNode {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode tA = headA;
+        ListNode tB = headB;
+        int count = 0;
+        while (tA!=tB&&count<3){
+            tA = tA.next;
+            tB = tB.next;
+            if(tA==null){
+                tA = headB;
+                count++;
+            }
+            if(tB==null){
+                tB = headA;
+                count++;
+            }
+        }
+        return count<3?tA:null;
+    }
+}
