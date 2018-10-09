acd6829dc347c87c5ca7a6f7e0050bb26200c08f
mjx <1032512598@qq.com>
2018-10-09 23:08:17 +0800

    add other solution

--- src/main/java/linkedlist/Palindrome.java
+++ src/main/java/linkedlist/Palindrome.java
@@ -5,7 +5,7 @@ public class Palindrome {
     private ListNode pre;
 
     public boolean isPalindrome(ListNode head) {
-        if(head==null||head.next==null){
+        if (head == null || head.next == null) {
             return true;
         }
         pre = head;
@@ -13,7 +13,7 @@ public class Palindrome {
     }
 
     private boolean helper(ListNode head) {
-        if(head==null){
+        if (head == null) {
             return true;
         }
         boolean ispd = helper(head.next);
@@ -21,5 +21,39 @@ public class Palindrome {
         if (ispd)
             pre = pre.next;
         return ispd;
+    }
+
+    //更好的做法
+    public boolean isPalindrome1(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast != null) fast = fast.next;
+        }
+        //此时slow走了一半，将后一半反转
+        ListNode reverseHead = reverse(slow);
+        while (reverseHead != null) {
+            if (reverseHead.val != head.val) return false;
+            reverseHead = reverseHead.next;
+            head = head.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        //反转链表的头节点
+        ListNode reverseHead = null;
+        //下一个要插入的节点
+        ListNode p = head;
+        while (p != null) {
+            ListNode tmp = p.next;
+            p.next = reverseHead;
+            reverseHead = p;
+            p = tmp;
+        }
+        return reverseHead;
     }
 }
