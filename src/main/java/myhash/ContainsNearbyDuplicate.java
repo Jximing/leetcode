c06b4edfbb1f7bf2dcd375e7a84d8d5bad562347
jxm <1032512598@qq.com>
2018-10-19 22:46:44 +0800

    test wrong method

--- src/main/java/myhash/ContainsNearbyDuplicate.java
+++ src/main/java/myhash/ContainsNearbyDuplicate.java
@@ -1,5 +1,8 @@
 package myhash;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContainsNearbyDuplicate {
 
     public boolean containsNearbyDuplicate(int[] nums, int k) {
@@ -17,5 +20,41 @@ public class ContainsNearbyDuplicate {
             }
         }
         return false;
+    }
+
+    public boolean containsNearbyDuplicate1(int[] nums, int k) {
+
+        if(k < 0 || nums.length > 1000)
+            return false;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                if(k >= i - map.get(nums[i])) return true;
+            }
+            map.put(nums[i], i);
+        }
+
+        return false;
+    }
+
+    public boolean containsNearbyDuplicate2(int[] nums, int k) {
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if(nums[i]>nums[j])
+                    break;
+                else if (nums[i] == nums[j]) {
+                    if((i-j)<=k)
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        ContainsNearbyDuplicate cnd = new ContainsNearbyDuplicate();
+        int[] nums = {9,2,9,4,3,1};
+        System.out.print(cnd.containsNearbyDuplicate2(nums,2));
     }
 }
