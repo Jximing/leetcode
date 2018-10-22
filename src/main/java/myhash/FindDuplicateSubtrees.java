a8f066ff2777094526502ce1a7ef9051d169b812
jxm <1032512598@qq.com>
2018-10-22 23:04:53 +0800

    modify method

--- src/main/java/myhash/FindDuplicateSubtrees.java
+++ src/main/java/myhash/FindDuplicateSubtrees.java
@@ -5,6 +5,7 @@ import sun.reflect.generics.tree.Tree;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FindDuplicateSubtrees {
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
@@ -16,7 +17,7 @@ public class FindDuplicateSubtrees {
         return list;
     }
 
-    public String find(TreeNode root, HashMap<String, Integer> map, List<TreeNode> list) {
+    private String find(TreeNode root, HashMap<String, Integer> map, List<TreeNode> list) {
         if (root == null){
             return "#";
         }
@@ -26,6 +27,28 @@ public class FindDuplicateSubtrees {
         }
         map.put(str, map.getOrDefault(str, 0) + 1);
         return str;
+    }
+
+    public List<TreeNode> findDuplicateSubtrees1(TreeNode root) {
+        List<TreeNode> res = new ArrayList<>();
+        Map<Long, Integer> id =  new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        helper(root, id, map, res);
+        return res;
+    }
+
+    private Integer helper(TreeNode root, Map<Long, Integer> id, Map<Integer, Integer> map, List<TreeNode> res) {
+        if (root == null) return 0;
+        Long key = ((long) root.val << 32) | helper(root.left, id, map, res) << 16 | helper(root.right, id, map, res);
+        if (!id.containsKey(key))
+            id.put(key, id.size() + 1);
+        int curId = id.get(key);
+        if (map.containsKey(curId)) {
+            if (map.get(curId) == 1) res.add(root);
+            map.put(curId, map.get(curId) + 1);
+        }
+        else map.put(curId, 1);
+        return curId;
     }
 }
 
