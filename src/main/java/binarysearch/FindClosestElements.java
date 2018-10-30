10487593761a7cd296fbce560a06f15e0e700378
Jximing <1032512598@qq.com>
2018-10-31 00:00:39 +0800

    add method

--- src/main/java/binarysearch/FindClosestElements.java
+++ src/main/java/binarysearch/FindClosestElements.java
@@ -30,6 +30,32 @@ public class FindClosestElements {
         return res;
     }
 
+    /*
+     * @Author jxm
+     * @Description 更优的解法
+     * @Date 23:55 2018/10/30
+     * @Param
+     * @return
+     **/
+    public List<Integer> findClosestElements1(int[] arr, int k, int x) {
+        List<Integer> list = new ArrayList<>();
+        //相当于查找左边应该留出多少位
+        int start = 0, end = arr.length - k;
+        while (start < end) {
+            //每次多跳一点
+            int mid = (start + end) / 2;
+            if (x - arr[mid] > arr[mid + k] - x) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+
+        }
+        for (int j = start; j < start + k; j++) {
+            list.add(arr[j]);
+        }
+        return list;
+    }
     public static void main(String[] args){
         FindClosestElements fce = new FindClosestElements();
         int[] arr = {0,0,1,2,3,3,4,7,7,8};
