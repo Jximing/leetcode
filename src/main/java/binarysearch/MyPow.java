381c7c5306c8306077406b506f0a82ae8c3530f4
jxm <1032512598@qq.com>
2018-11-01 13:53:18 +0800

    add method

--- src/main/java/binarysearch/MyPow.java
+++ src/main/java/binarysearch/MyPow.java
@@ -26,6 +26,18 @@ public class MyPow {
         return pow;
     }
 
+
+    public  double myPow1(double x, int n) {
+        if (n == 0) return 1;
+        if(n<0) return 1/x * myPow(1/x, -(n+1));
+        if (n == 1) return x;
+        if (n == 2) return x*x;
+
+        int absN = n;
+
+        return (n%2 == 0) ? myPow(myPow(x, n/2),2):x*myPow(myPow(x, n/2),2);
+    }
+
     public static void main(String[] args){
         MyPow mp = new MyPow();
         System.out.println(mp.myPow(2.0,2));
