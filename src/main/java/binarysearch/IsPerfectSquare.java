1311b6f166b4c3ac924426030986687019a3b308
jxm <1032512598@qq.com>
2018-11-02 09:31:00 +0800

    add isperfectsquare

---
+++ src/main/java/binarysearch/IsPerfectSquare.java
@@ -0,0 +1,23 @@
+package binarysearch;
+
+public class IsPerfectSquare {
+
+    public boolean isPerfectSquare(int num) {
+        if(num==1){
+            return true;
+        }
+        int left = 0;
+        int right = num;
+        while (left<=right){
+            int mid = left + (right - left)/2;
+            if(num%mid==0&&mid==num/mid){
+                return true;
+            }else if(mid>num/mid){
+                right = mid-1;
+            }else {
+                left = mid+1;
+            }
+        }
+        return false;
+    }
+}
