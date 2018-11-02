818aad37a4313417ef7741ef2b0143deabdbc202
Jximing <1032512598@qq.com>
2018-11-02 22:22:48 +0800

    add method

--- src/main/java/binarysearch/FindMin.java
+++ src/main/java/binarysearch/FindMin.java
@@ -29,9 +29,41 @@ public class FindMin {
         }
     }
 
+
+    public int findMin1(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        int start =0;
+        int end = nums.length-1;
+        //第一个数小于最后一个数，说明是完全升序
+        if (nums[start]<nums[end])
+            return nums[start];
+
+        int middle=0;
+        while (start<=end){
+            middle = start +(end-start)/2; //需要保证不能 int 相加出现溢出
+
+            //让中位数和右边的数字比较
+            if (nums[middle]>nums[end]){
+                start = middle+1;
+            }
+            //如果 end = middle-1 的话，不能通过 {3,1,2}
+            else if (nums[middle]<nums[end]){
+                end = middle;//因为这里的 end 可能就是最小值，不能加一
+            }
+            //如果中间和最后相等。怎么查找，主要前后移动方向不确定
+            //无法确定的时候，让 右边的值自减就好了
+            else {
+                end--;
+            }
+        }
+        return nums[start];
+    }
+
     public static void main(String[] args){
         FindMin fm = new FindMin();
-        int[] nums = {3,4,5,1,2};
-        System.out.print(fm.findMin(nums));
+        int[] nums = {3,3,1,3};
+        System.out.print(fm.findMin1(nums));
     }
 }
