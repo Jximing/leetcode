5869f573038a6e6a3e661dc87eeaa1cd023dcb45
Jximing <1032512598@qq.com>
2018-11-03 22:10:31 +0800

    add method

--- src/main/java/binarysearch/FindDuplicate.java
+++ src/main/java/binarysearch/FindDuplicate.java
@@ -38,6 +38,27 @@ public class FindDuplicate {
         return slow;
     }
 
+    public int findDuplicate2(int[] nums) {
+        int left = 1;
+        int right = nums.length-1;
+        while (left<right){
+            int mid = left+(right-left)/2;
+            int count = 0;
+            for (int n:nums){
+                if(n<mid){
+                    count++;
+                }
+            }
+            if(count<=mid){
+                left=mid+1;
+            }else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+
     public static void main(String[] args){
         FindDuplicate fd = new FindDuplicate();
         int[] nums = {1,2,6,7,9,8,5,4,3,5,5};
