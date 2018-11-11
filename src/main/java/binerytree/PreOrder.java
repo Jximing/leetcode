b867b22d028d008a485a07bdf98223ae3130b158
jxm <1032512598@qq.com>
2018-11-11 21:53:30 +0800

    add binerytree

---
+++ src/main/java/binerytree/PreOrder.java
@@ -0,0 +1,34 @@
+package binerytree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PreOrder {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root,res);
+        return res;
+    }
+
+    private void helper(TreeNode root, List<Integer> res) {
+        if(root==null)
+            return;
+        res.add(root.val);
+        if (root.left != null)
+            helper(root.left, res);
+        if (root.right != null)
+            helper(root.right, res);
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
