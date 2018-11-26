0fc93520512433c2c01b89c6cc613d75dc82f484
mjx <1032512598@qq.com>
2018-11-26 23:59:55 +0800

    temp

--- src/main/java/binerytree/Codec.java
+++ src/main/java/binerytree/Codec.java
@@ -59,6 +59,7 @@ public class Codec {
                 TreeNode newnode = null;
                 if(!cols[j].equals("null")){
                     newnode = new TreeNode(Integer.valueOf(cols[j]));
+                    
                 }
             }
         }
