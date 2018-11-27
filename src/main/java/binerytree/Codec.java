00ca5bf9975fd8e93c3ca4ce462029eff6d174b9
mjx <1032512598@qq.com>
2018-11-28 00:40:26 +0800

    finish method

--- src/main/java/binerytree/Codec.java
+++ src/main/java/binerytree/Codec.java
@@ -1,7 +1,6 @@
 package binerytree;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
 
 /**
  * @ClassName Codec
@@ -12,71 +11,79 @@ import java.util.Queue;
  **/
 public class Codec {
 
-    // Encodes a tree to a single string.
+    /**
+     * This method will be invoked first, you should design your own algorithm
+     * to serialize a binary tree which denote by a root node to a string which
+     * can be easily deserialized by your own "deserialize" method later.
+     */
     public String serialize(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        Queue<TreeNode> helper = new LinkedList<>();
-        Queue<TreeNode> temp = null;
-        queue.offer(root);
-        String str = root.val+"|";
-        while (!queue.isEmpty()){
-            String row = "";
-            while (!queue.isEmpty()){
-                TreeNode cur = queue.poll();
-                if (cur==null){
-                    helper.offer(null);
-                    helper.offer(null);
-                    row=row+"null,null,";
-                }else {
-                    helper.offer(cur.left);
-                    helper.offer(cur.right);
-
-                    row=row+(cur.left==null?"null":cur.left.val)
-                            +","+(cur.right==null?"null":cur.right.val)+",";
-                }
-            }
-            if(row.replaceAll("null,","").equals("")){
-                break;
-            }
-            str = str + row.substring(0,row.length()-1)+"|";
-            temp = queue;
-            queue = helper;
-            helper = temp;
+        // write your code here
+        if( root == null)
+            return "{}";
+        ArrayList<TreeNode> queue = new ArrayList<>();
+        queue.add(root);
+        // 将二叉树的个节点按照从上到下、从左到有的存储在queue中
+        for(int i=0;i<queue.size();i++){
+            TreeNode q = queue.get(i);
+            if(q== null)
+                continue;
+            queue.add(q.left);
+            queue.add(q.right);
         }
-        return str;
+        // 去除叶子节点的左右孩子，这个孩子是空值
+        while(queue.get(queue.size() - 1) == null){
+            queue.remove(queue.size() - 1);
+        }
+        // 遍历queue把转换成字符串
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        sb.append(queue.get(0).val);
+        for(int i=1;i<queue.size(); i++){
+            TreeNode q = queue.get(i);
+            if(q!= null){
+                sb.append(",");
+                sb.append(q.val);
+            }else{
+                sb.append(",#");
+            }
+        }
+        sb.append("}");
+        return sb.toString();
     }
 
-    // Decodes your encoded data to tree.
+    /**
+     * This method will be invoked second, the argument data is what exactly
+     * you serialized at method "serialize", that means the data is not given by
+     * system, it's given by your own serialize method. So the format of data is
+     * designed by yourself, and deserialize it here as you serialize it in
+     * "serialize" method.
+     */
     public TreeNode deserialize(String data) {
-        if(data==null||data.length()==0){
+        // write your code here
+        if(data == "{}")
             return null;
-        }
-        String[] rows = data.split("\\|");
-        TreeNode root = new TreeNode(Integer.valueOf(rows[0]));
-        if(rows.length==1){
-            return root;
-        }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        TreeNode cur = null;
-        for (int i = 1;i<rows.length;i++){
-            String[] cols = rows[i].split(",");
-            cur = queue.poll();
-            for (int j=0;j<cols.length;j++){
-                TreeNode newnode = null;
-                if(!cols[j].equals("null")){
-                    newnode = new TreeNode(Integer.valueOf(cols[j]));
+        // 以逗号分割
+        String[] vals = data.substring(1,data.length()-1).split(",");
+        ArrayList<TreeNode> queue = new ArrayList<>();
+        // 根节点
+        TreeNode root = new TreeNode(Integer.parseInt(vals[0]));
+        queue.add(root);
+        int index = 0;
+        boolean isLeftChild = true;
+        for (int i = 1; i < vals.length; i++) {
+            if (!vals[i].equals("#")) {
+                TreeNode node = new TreeNode(Integer.parseInt(vals[i]));
+                if (isLeftChild) {
+                    queue.get(index).left = node;
+                } else {
+                    queue.get(index).right = node;
                 }
-                if(j%2==0){
-                    cur.left = newnode;
-                }else {
-                    cur.right = newnode;
-                    if(j<cols.length-1){
-                        cur = queue.poll();
-                    }
-                }
-                queue.offer(newnode);
+                queue.add(node);
             }
+            if (!isLeftChild) {
+                index++;
+            }
+            isLeftChild = !isLeftChild;
         }
         return root;
     }
@@ -85,9 +92,15 @@ public class Codec {
     public static void main(String[] args){
         Codec codec = new Codec();
         TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
+//        root.left = new TreeNode(2);
         root.right = new TreeNode(3);
-        root.right.right = new TreeNode(4);
+//        root.right.left = new TreeNode(4);
+//        root.right.right = new TreeNode(5);
+//        root.right.left.left = new TreeNode(6);
+//        root.right.left.right = new TreeNode(7);
+//        root.right = new TreeNode(3);
+//        root.right.left = new TreeNode(4);
+//        root.right.right = new TreeNode(5);
         String str = codec.serialize(root);
         System.out.println(str);
 
