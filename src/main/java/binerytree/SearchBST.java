2f9e50793e63f3f1978761c5d285af614e06f302
mjx <1032512598@qq.com>
2018-11-29 00:28:15 +0800

    add SearchBST

---
+++ src/main/java/binerytree/SearchBST.java
@@ -0,0 +1,26 @@
+package binerytree;
+
+/**
+ * @ClassName SearchBST
+ * @Description Search in a Binary Search Tree
+ * @Author mingjie
+ * @Date 2018/11/29 12:22 AM
+ * @Versrion 1.0
+ **/
+public class SearchBST {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root==null){
+            return null;
+        }
+        while (root!=null&&root.val!=val){
+            if(root.val>val){
+                root=root.left;
+            }else if(root.val<val){
+                root = root.right;
+            }
+        }
+        return root;
+    }
+
+}
