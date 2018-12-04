62a34c447c5f520e81d7531dd28d3682073e2351
mjx <1032512598@qq.com>
2018-12-05 00:21:11 +0800

    add insertintobst

---
+++ src/main/java/binerytree/InsertIntoBST.java
@@ -0,0 +1,32 @@
+package binerytree;
+
+/**
+ * @ClassName InsertIntoBST
+ * @Description Insert into a Binary Search Tree
+ * @Author mingjie
+ * @Date 2018/12/4 11:46 PM
+ * @Versrion 1.0
+ **/
+public class InsertIntoBST {
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root.val>val&&(root.left==null||root.left.val<val)){
+            TreeNode cur = new TreeNode(val);
+            cur.left = root.left;
+            root.left = cur;
+        }
+        if(root.val<val&&root.right==null){
+            TreeNode cur = new TreeNode(val);
+            cur.right = root.right;
+            root.right = cur;
+        }
+        if(root.val>val){
+            root.left = insertIntoBST(root.left,val);
+        }
+        if(root.val<val){
+            root.right = insertIntoBST(root.right,val);
+        }
+        return root;
+    }
+
+}
