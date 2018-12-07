8bc5a88add20e1502c2fdbcf92a4c928bd32353d
Jximing <1032512598@qq.com>
2018-12-07 21:49:08 +0800

    add method

--- src/main/java/binerytree/KthLargest.java
+++ src/main/java/binerytree/KthLargest.java
@@ -15,26 +15,26 @@ public class KthLargest {
 
     int[] data;
 
-    public KthLargest(int k, int[] nums) {
-        this.kth = k;
-        this.data = new int[k];
-        if (nums != null&&nums.length!=0) {
-            Arrays.sort(nums);
-            if(k>nums.length){
-                int length = nums.length;
-                for (int i=k-1;i>=0;i--){
-                    if(length==0){
-                        data[i] = Integer.MIN_VALUE;
-                    }else {
-                        data[i]=nums[--length];
-                    }
-                }
-            }
-            for (int i = nums.length - 1; kth > 0&&i>=0; i--) {
-                data[--kth] = nums[i];
-            }
-        }
-    }
+//    public KthLargest(int k, int[] nums) {
+//        this.kth = k;
+//        this.data = new int[k];
+//        if (nums != null&&nums.length!=0) {
+//            Arrays.sort(nums);
+//            if(k>nums.length){
+//                int length = nums.length;
+//                for (int i=k-1;i>=0;i--){
+//                    if(length==0){
+//                        data[i] = Integer.MIN_VALUE;
+//                    }else {
+//                        data[i]=nums[--length];
+//                    }
+//                }
+//            }
+//            for (int i = nums.length - 1; kth > 0&&i>=0; i--) {
+//                data[--kth] = nums[i];
+//            }
+//        }
+//    }
 
     public int add(int val) {
         if(kth!=0){
@@ -57,15 +57,59 @@ public class KthLargest {
         return data[0];
     }
 
-    public static void main(String[] args){
-        int k = 3;
-        int[] arr = {5,-1};
-        KthLargest kthLargest = new KthLargest(k, arr);
-        kthLargest.add(2);   // returns 4
-        kthLargest.add(1);   // returns 5
-        kthLargest.add(-1);  // returns 5
-        kthLargest.add(3);   // returns 8
-        kthLargest.add(4);   // returns 8
+
+    /*
+     * @Author jxm
+     * @Description //其它方法
+     * @Date 0:57 2018/12/2
+     * @Param
+     * @return
+     **/
+
+    private final int[] heap;
+    private final int k;
+    private int size;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.heap = new int[k + 1];
+        for (int n : nums) add1(n);
+    }
+
+    public int add1(int val) {
+        if (size < k) {
+            // Build a min heap from the first k elements.
+            heap[++size] = val;
+            // Promote val to the top.
+            for (int i = size; i > 1 && heap[i] < heap[i >>> 1];) swap(i, i >>>= 1);
+        } else if (val > heap[1]) {
+            // If val is greater than the minimum then it replaces the minimum.
+            // Otherwise it's ignored.
+            heap[1] = val;
+            // Move val to the bottom.
+            sink();
+        }
+        return heap[1];
+    }
+
+    private void sink() {
+        int i = 1;
+        while (i << 1 <= size) {
+            // 2i and 2i+1 are the children of i.
+            int j = i << 1;
+            // If 2i+1 exists, choose the smallest of 2i and 2i+1.
+            if (j < size && heap[j + 1] < heap[j]) j++;
+            // If there are no children less than i then we're done.
+            if (heap[i] <= heap[j]) break;
+            // Otherwise swap i and the smallest of its children.
+            swap(i, i = j);
+        }
+    }
+
+    private void swap(int i, int j) {
+        int t = heap[i];
+        heap[i] = heap[j];
+        heap[j] = t;
     }
 
 }
