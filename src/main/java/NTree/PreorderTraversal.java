1246c991a0b54d77e14bf93fd9bb0ceb3c57e671
mjx <1032512598@qq.com>
2018-12-10 00:13:37 +0800

    add method

--- src/main/java/NTree/PreorderTraversal.java
+++ src/main/java/NTree/PreorderTraversal.java
@@ -2,6 +2,7 @@ package NTree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @ClassName PreorderTraversal
@@ -28,6 +29,50 @@ public class PreorderTraversal {
                 helper(node,list);
             }
         }
+    }
+    /**
+     * @Author mingjie
+     * @Description //递归方法2
+     * @Date 12:12 AM 2018/12/10
+     * @Param [root]
+     * @return java.util.List<java.lang.Integer>
+     **/
+    public List<Integer> preorder1(Node root) {
+        List<Integer> list = new ArrayList<Integer>();
+        if(root==null) return list;
+        list.add(root.val);
+        if(root.children!=null){
+            for(Node node:root.children){
+                List<Integer> list2 = preorder1(node);
+                list.addAll(list2);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * @Author mingjie
+     * @Description //迭代方法
+     * @Date 12:12 AM 2018/12/10
+     * @Param [root]
+     * @return java.util.List<java.lang.Integer>
+     **/
+    public List<Integer> preorder2(Node root) {
+        Stack<Node> stack=new Stack<>();
+        stack.push(root);
+        ArrayList<Integer> result=new ArrayList<>();
+        if(root==null)
+            return result;
+        while(!stack.isEmpty()){
+            Node cur=stack.pop();
+            result.add(cur.val);
+            if(cur.children!=null){
+                for(int i=cur.children.size()-1;i>=0;i--){
+                    stack.push(cur.children.get(i));
+                }
+            }
+        }
+        return result;
     }
 }
 class Node {
