7cfb351bffe343436e320bd6d5445f88f895dce0
jxm <1032512598@qq.com>
2018-12-11 01:23:18 +0800

    test idea commit

---
+++ src/main/java/NTree/PostorderTraversal.java
@@ -0,0 +1,19 @@
+package NTree;
+
+import java.util.List;
+
+/**
+ * @ClassName PostorderTraversal
+ * @Description TODO
+ * @Author mingjie
+ * @Date 2018/12/10 9:07 PM
+ * @Versrion 1.0
+ **/
+public class PostorderTraversal {
+
+    public List<Integer> postorder(Node root) {
+
+        return null;
+    }
+
+}
