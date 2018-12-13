41d950f509b25fd70b511e3736dffef13a0a8af4
mjx <1032512598@qq.com>
2018-12-13 23:34:28 +0800

    add method

--- src/main/java/secondstage/array/MaxProfit.java
+++ src/main/java/secondstage/array/MaxProfit.java
@@ -26,6 +26,19 @@ public class MaxProfit {
         return profit;
     }
 
+    public int maxProfit1(int[] prices) {
+        if(prices == null || prices.length <=1) {
+            return 0;
+        }
+        int max = 0, tmp;
+        // 没有买入卖出的限制，直接和第二天比较价格就是利润
+        for(int i=0;i<prices.length-1; i++) {
+            if ((tmp = prices[i + 1] - prices[i]) > 0) {
+                max += tmp;
+            }
+        }
+        return max;
+    }
 
     public static void main(String[] args){
         int[] prices = {7,1,5,3,6,4};
