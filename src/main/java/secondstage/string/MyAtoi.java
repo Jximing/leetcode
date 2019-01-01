037a40a07dd19d39a7d94ed444f9797940d4974c
mjx <1032512598@qq.com>
2019-01-01 09:50:03 +0800

    add method

--- src/main/java/secondstage/string/MyAtoi.java
+++ src/main/java/secondstage/string/MyAtoi.java
@@ -1,5 +1,6 @@
 package secondstage.string;
 
+
 /**
  * @ClassName MyAtoi
  * @Description 字符串转换整数 (atoi)
@@ -10,18 +11,24 @@ package secondstage.string;
 public class MyAtoi {
 
     public int myAtoi(String str) {
-        char[] arr = str.toCharArray();
-        int i = 0;
-        for (; i < arr.length; i++) {
-            if (arr[i] == '-' || arr[i] == '+' || arr[i] >= '0' && arr[i] <= '9') {
-                break;
+        if (str.trim().isEmpty()) return 0;
+        int sign = 1, base = 0, i = 0, n = str.length();
+        while (i < n && str.charAt(i) == ' ') ++i;
+        if (str.charAt(i) == '+' || str.charAt(i) == '-') {
+            sign = (str.charAt(i++) == '+') ? 1 : -1;
+        }
+        while (i < n && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+            if (base > Integer.MAX_VALUE / 10 || (base == Integer.MAX_VALUE / 10 && str.charAt(i) - '0' > 7)) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
+            base = 10 * base + (str.charAt(i++) - '0');
         }
-        int num = 0;
-        for (; i < arr.length; i++) {
+        return base * sign;
+    }
 
-            num += arr[i] * 10;
-        }
-        return num;
+    public static void main(String[] args) {
+        MyAtoi ma = new MyAtoi();
+        System.out.print(ma.myAtoi(" "));
+
     }
 }
